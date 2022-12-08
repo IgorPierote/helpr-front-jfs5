a3bfacb6ccaaea56efb40fe372ec03420a9784db
raquelnob <105522347+RaquelNob@users.noreply.github.com>
2022-12-07 21:33:35 -0300

    Criação de Títulos para cada página e definição de rota padrão

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,41 @@
+import { ChamadosComponent } from './views/chamados/chamados/chamados.component';
+import { ClientesComponent } from './views/clientes/clientes/clientes.component';
+import { HomeComponent } from './views/home/home/home.component';
+import { LoginComponent } from './views/login/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   {
+    path: '',
+    component: HomeComponent,
+    data: { titulo: 'Helpr | Página Principal'}
+  },
+  {
     path: 'login',
-    loadChildren: () => import("./views/login/login.module").then(m => m.LoginModule)
+    loadChildren: () => import("./views/login/login.module").then(m => m.LoginModule),
+    component: LoginComponent,
+    data: {titulo: 'Login'}
   },
   {
     path: 'home',
     loadChildren: () => import('./views/home/home.module').then(m => m.HomeModule),
-    canActivate: [ AuthGuard ]
+    canActivate: [ AuthGuard ],
+    component: HomeComponent,
+    data: { titulo: 'Helpr | Página Principal'}
   },
   {
     path: 'clientes',
-    loadChildren: () => import('./views/clientes/clientes.module').then(m => m.ClientesModule)
+    loadChildren: () => import('./views/clientes/clientes.module').then(m => m.ClientesModule),
+    component: ClientesComponent,
+    data: { titulo: 'Helpr | Clientes'}
   },
   {
     path: 'chamados',
-    loadChildren: () => import('./views/chamados/chamados.module').then(m => m.ChamadosModule)
+    loadChildren: () => import('./views/chamados/chamados.module').then(m => m.ChamadosModule),
+    component: ChamadosComponent,
+    data: { titulo: 'Helpr | Chamados'}
   }
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { filter, map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'helpr-front';
+  constructor(
+    activatedRoute: ActivatedRoute,
+    router: Router,
+    titleService: Title
+  ) {
+    router.events.pipe(filter((event) => event instanceof NavigationEnd)).pipe(map(() => activatedRoute)).pipe(
+      map((route) => {
+        while (route.firstChild) route = route.firstChild;
+        return route;
+      })
+    ).pipe(switchMap((route) => route.data)).subscribe((event) => titleService.setTitle(event['titulo']));
+  }
 }
