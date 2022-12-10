d733dd9a7fbd3c529cd2ad33a6fed33d96f293a4
Andrea c. Baron <andrea.martensc@gmail.com>
2022-12-09 21:54:16 -0300

    TR percorrer a tabela com filtro

--- src/app/views/clientes/clientes/clientes.component.ts
+++ src/app/views/clientes/clientes/clientes.component.ts
@@ -1,41 +1,55 @@
-import { ClienteService } from './../../../services/cliente.service';
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from 'src/app/models/cliente';
+import { ClienteService } from './../../../services/cliente.service';
 import { NotificationService } from 'src/app/services/notification.service';
+import { MatTableDataSource } from '@angular/material/table';
+
+const ELEMENT_DATA: Cliente[] = [];
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
-  styleUrls: ['./clientes.component.css']
+  styleUrls: ['./clientes.component.css'],
 })
 export class ClientesComponent implements OnInit {
-
-  displayedColumns: string[] = ['id', 'nome', 'cpf', 'email', 'telefone', 'editar', 'excluir'];
-  dataSource: Cliente[] = [];
+  displayedColumns: string[] = [
+    'id',
+    'nome',
+    'cpf',
+    'email',
+    'telefone',
+    'editar',
+    'excluir',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
   constructor(
     private clienteService: ClienteService,
     private notifyService: NotificationService
-    ) { }
+  ) {}
 
   ngOnInit(): void {
-   this.initializeTable()
-    
+    this.initializeTable();
   }
 
   private initializeTable(): void {
-    this.clienteService.findAll().subscribe(clientes => {
-      this.dataSource = clientes;
+    this.clienteService.findAll().subscribe((clientes) => {
+      this.dataSource = new MatTableDataSource(clientes);
     });
   }
 
   public delete(id: number): void {
-    let ok = confirm("Tem certeza que deseja excluir?");
-    if(ok) {
+    let ok = confirm('Tem certeza que deseja excluir?');
+    if (ok) {
       this.clienteService.delete(id).subscribe(() => {
-        this.notifyService.showSuccess("Cliente excluido.", "Sucesso :)");
+        this.notifyService.showSuccess('Cliente excluido.', 'Sucesso :)');
         this.initializeTable();
       });
     }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 }
--- src/app/views/funcionarios/funcionarios/funcionarios.component.ts
+++ src/app/views/funcionarios/funcionarios/funcionarios.component.ts
@@ -1,26 +1,42 @@
+import { MatTableDataSource } from '@angular/material/table';
 import { FuncionarioService } from 'src/app/services/funcionario.service';
 import { Funcionario } from './../../../models/funcionario';
 import { Component, OnInit } from '@angular/core';
 
+const ELEMENT_DATA: Funcionario[] = [];
+
 @Component({
   selector: 'app-funcionarios',
   templateUrl: './funcionarios.component.html',
-  styleUrls: ['./funcionarios.component.css']
+  styleUrls: ['./funcionarios.component.css'],
 })
 export class FuncionariosComponent implements OnInit {
+  displayedColumns: string[] = [
+    'foto',
+    'id',
+    'nome',
+    'cpf',
+    'email',
+    'cargo',
+    'editar',
+    'detalhes',
+  ];
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  constructor(private funcionarioService:FuncionarioService) { }
-  displayedColumns: string[] = ['foto','id', 'nome', 'cpf', 'email', 'cargo', 'editar', 'detalhes'];
-  dataSource: Funcionario[] = [];
+  constructor(private funcionarioService: FuncionarioService) {}
 
   ngOnInit(): void {
-    this.InitializeTable()
+    this.InitializeTable();
   }
-public InitializeTable(){
-  this.funcionarioService.findAll().subscribe(
-    resposta =>{
-      this.dataSource = resposta
-    }
-  )
-}
+
+  public InitializeTable() {
+    this.funcionarioService.findAll().subscribe((funcionario) => {
+      this.dataSource = new MatTableDataSource(funcionario);
+    });
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
